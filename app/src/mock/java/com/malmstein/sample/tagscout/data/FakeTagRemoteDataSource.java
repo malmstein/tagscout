daadf44962240cda534260a1c40717ffa1ca15bd
David Gonzalez <malmstein@gmail.com>
2016-04-02 15:04:39 +0100

    simplifying the fake data source

--- app/src/mock/java/com/malmstein/sample/tagscout/data/FakeTagRemoteDataSource.java
+++ app/src/mock/java/com/malmstein/sample/tagscout/data/FakeTagRemoteDataSource.java
@@ -1,57 +1,44 @@
 package com.malmstein.sample.tagscout.data;
 
-import android.content.res.AssetManager;
 import android.support.annotation.NonNull;
 
 import com.google.gson.Gson;
-import com.malmstein.sample.tagscout.data.model.GsonTag;
+import com.google.gson.reflect.TypeToken;
+import com.malmstein.sample.tagscout.data.model.Tag;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FakeTagRemoteDataSource implements TagDataSource {
 
     private static FakeTagRemoteDataSource INSTANCE;
 
-    private final AssetManager assetManager;
     private final Gson gson;
 
-    public static FakeTagRemoteDataSource getInstance(AssetManager assetManager, Gson gson) {
+    public static FakeTagRemoteDataSource getInstance() {
         if (INSTANCE == null) {
-            INSTANCE = new FakeTagRemoteDataSource(assetManager, gson);
+            INSTANCE = new FakeTagRemoteDataSource(new Gson());
         }
         return INSTANCE;
     }
 
     // Prevent direct instantiation.
-    private FakeTagRemoteDataSource(AssetManager assetManager, Gson gson) {
-        this.assetManager = assetManager;
+    private FakeTagRemoteDataSource(Gson gson) {
         this.gson = gson;
     }
 
     @Override
     public void getTags(@NonNull LoadTagsCallback callback) {
-        GsonTag value;
+        List<Tag> value;
+        Type listType = new TypeToken<ArrayList<Tag>>() {}.getType();
         try {
-            value = readFileAs("tags.json", GsonTag.class);
-        } catch (IOException e) {
+            value = gson.fromJson(FakeJson.tags, listType);
+        } catch (Exception e) {
             callback.onDataNotAvailable();
             return;
         }
-        callback.onTagsLoaded(value.tags);
+        callback.onTagsLoaded(value);
     }
 
-    private <T> T readFileAs(String file, Class<T> type) throws IOException {
-        InputStream is = null;
-        try {
-            is = assetManager.open("json/" + file);
-            return gson.fromJson(new InputStreamReader(is, Charset.forName("UTF-8")), type);
-        } finally {
-            if (is != null) {
-                is.close();
-            }
-        }
-    }
 }
