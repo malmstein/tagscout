94d9640f23d036ff1fb20a07cee71c5fbbf5dae4
David Gonzalez <malmstein@gmail.com>
2016-04-02 14:16:18 +0100

    the fake data source reads a local json file

---
+++ app/src/main/java/com/malmstein/sample/tagscout/data/model/GsonTag.java
@@ -0,0 +1,12 @@
+package com.malmstein.sample.tagscout.data.model;
+
+import java.util.List;
+
+public class GsonTag {
+
+    public final List<Tag> tags;
+
+    public GsonTag(List<Tag> tags) {
+        this.tags = tags;
+    }
+}
--- app/src/mock/java/com/malmstein/sample/tagscout/data/FakeTagRemoteDataSource.java
+++ app/src/mock/java/com/malmstein/sample/tagscout/data/FakeTagRemoteDataSource.java
@@ -1,4 +1,57 @@
 package com.malmstein.sample.tagscout.data;
 
-public class FakeTagRemoteDataSource {
+import android.content.res.AssetManager;
+import android.support.annotation.NonNull;
+
+import com.google.gson.Gson;
+import com.malmstein.sample.tagscout.data.model.GsonTag;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
+public class FakeTagRemoteDataSource implements TagDataSource {
+
+    private static FakeTagRemoteDataSource INSTANCE;
+
+    private final AssetManager assetManager;
+    private final Gson gson;
+
+    public static FakeTagRemoteDataSource getInstance(AssetManager assetManager, Gson gson) {
+        if (INSTANCE == null) {
+            INSTANCE = new FakeTagRemoteDataSource(assetManager, gson);
+        }
+        return INSTANCE;
+    }
+
+    // Prevent direct instantiation.
+    private FakeTagRemoteDataSource(AssetManager assetManager, Gson gson) {
+        this.assetManager = assetManager;
+        this.gson = gson;
+    }
+
+    @Override
+    public void getTags(@NonNull LoadTagsCallback callback) {
+        GsonTag value;
+        try {
+            value = readFileAs("tags.json", GsonTag.class);
+        } catch (IOException e) {
+            callback.onDataNotAvailable();
+            return;
+        }
+        callback.onTagsLoaded(value.tags);
+    }
+
+    private <T> T readFileAs(String file, Class<T> type) throws IOException {
+        InputStream is = null;
+        try {
+            is = assetManager.open("json/" + file);
+            return gson.fromJson(new InputStreamReader(is, Charset.forName("UTF-8")), type);
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
 }
