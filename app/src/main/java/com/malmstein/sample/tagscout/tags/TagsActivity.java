c8d8e093847a0befb59fa8d4009a95377fc2feaa
David Gonzalez <malmstein@gmail.com>
2016-04-03 15:53:45 +0100

    loading tasks the first time we are launching

--- app/src/main/java/com/malmstein/sample/tagscout/tags/TagsActivity.java
+++ app/src/main/java/com/malmstein/sample/tagscout/tags/TagsActivity.java
@@ -4,12 +4,25 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.malmstein.sample.tagscout.R;
+import com.malmstein.sample.tagscout.injection.Injection;
 
 public class TagsActivity extends AppCompatActivity {
+
+    private TagsPresenter tagsPresenter;
+    private TagsView tagsView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tags);
+
+        tagsView = (TagsView) findViewById(R.id.tags_view);
+
+        tagsPresenter = new TagsPresenter(Injection.provideUseCaseHandler(), Injection.provideRetrieveTagsUseCase(), tagsView);
+
+        if (savedInstanceState == null) {
+            tagsPresenter.loadTags();
+        }
     }
+
 }
--- app/src/main/java/com/malmstein/sample/tagscout/tags/TagsView.java
+++ app/src/main/java/com/malmstein/sample/tagscout/tags/TagsView.java
@@ -1,13 +1,17 @@
 package com.malmstein.sample.tagscout.tags;
 
 import android.content.Context;
+import android.support.design.widget.Snackbar;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.widget.FrameLayout;
 
 import com.malmstein.sample.tagscout.R;
+import com.malmstein.sample.tagscout.data.model.Tag;
 
-public class TagsView extends FrameLayout {
+import java.util.List;
+
+public class TagsView extends FrameLayout implements TagsContract.View{
 
     public TagsView(Context context) {
         super(context);
@@ -28,4 +32,13 @@ public class TagsView extends FrameLayout {
         LayoutInflater.from(getContext()).inflate(R.layout.view_tags_view, this, true);
     }
 
+    @Override
+    public void showTags(List<Tag> tags) {
+        Snackbar.make(this, "Showing " + tags.size() + " tags", Snackbar.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void showLoadingTagsError() {
+        Snackbar.make(this, "Couldn't load tags", Snackbar.LENGTH_LONG).show();
+    }
 }
