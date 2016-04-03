6c7b573290c2d313316d441c1ef6c8110375006c
David Gonzalez <malmstein@gmail.com>
2016-04-03 15:34:20 +0100

    added skeleton Activity with default theming

---
+++ app/src/main/java/com/malmstein/sample/tagscout/tags/TagsActivity.java
@@ -0,0 +1,15 @@
+package com.malmstein.sample.tagscout.tags;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.malmstein.sample.tagscout.R;
+
+public class TagsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tags);
+    }
+}
