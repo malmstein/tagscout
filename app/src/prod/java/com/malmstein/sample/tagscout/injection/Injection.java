7bfa5df760408c8a179b93320b6e1abb6f2d520b
David Gonzalez <malmstein@gmail.com>
2016-04-03 14:29:48 +0100

    adding remote data source injection to the prod flavour

---
+++ app/src/prod/java/com/malmstein/sample/tagscout/injection/Injection.java
@@ -0,0 +1,27 @@
+package com.malmstein.sample.tagscout.injection;
+
+import com.malmstein.sample.tagscout.data.RemoteTagDataSource;
+import com.malmstein.sample.tagscout.data.TagRepository;
+import com.malmstein.sample.tagscout.domain.UseCaseHandler;
+import com.malmstein.sample.tagscout.tags.GetTags;
+
+/**
+ * Enables injection of mock implementations for
+ * {@link com.malmstein.sample.tagscout.data.TagDataSource} at compile time.
+ * This is useful for testing, since it allows us to use a fake instance of the class to isolate the dependencies and run a test hermetically.
+ */
+public class Injection {
+
+    public static TagRepository provideTagRepository() {
+        return TagRepository.getInstance(RemoteTagDataSource.getInstance());
+    }
+
+    public static UseCaseHandler provideUseCaseHandler() {
+        return UseCaseHandler.getInstance();
+    }
+
+    public static GetTags provideGetTags() {
+        return new GetTags(Injection.provideTagRepository());
+    }
+
+}
