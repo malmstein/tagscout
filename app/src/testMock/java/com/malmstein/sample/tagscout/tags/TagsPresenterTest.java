c25ae860ee423e0fe94f8baa112fa234a7ecb2eb
David Gonzalez <malmstein@gmail.com>
2016-04-10 01:28:26 +0100

    clean up

--- app/src/testMock/java/com/malmstein/sample/tagscout/tags/TagsPresenterTest.java
+++ app/src/testMock/java/com/malmstein/sample/tagscout/tags/TagsPresenterTest.java
@@ -20,7 +20,6 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.verify;
 
 public class TagsPresenterTest {
